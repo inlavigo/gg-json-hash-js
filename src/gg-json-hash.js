60ce93293e28034eba64ef4be0e14eecb6294f08
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2024-11-29 07:08:35 +0100

    Add implementation from dart.

--- src/gg-json-hash.js
+++ src/gg-json-hash.js
@@ -1,11 +1,245 @@
-/**
- * Json hash is calculated
- * @param {string} x
- */
-export function ggJsonHash(x) {
-  console.log('ggJsonHash!' + x);
+// @license
+// Copyright (c) 2019 - 2024 Dr. Gabriel Gatzsche. All Rights Reserved.
+//
+// Use of this source code is governed by terms that can be
+// found in the LICENSE file in the root of this package.
 
-  if (x === '10') {
-    return 'undefined';
+// #############################################################################
+import crypto from 'crypto';
+
+/**
+ * Deeply hashes a JSON object.
+ * @param {object} json - The JSON object to hash.
+ * @param {boolean} updateExistingHashes - Replace existing hashes.
+ * @param {number} floatingPointPrecision - Precision for floating point numbers.
+ * @param {number} hashLength - The length of the hash.
+ * @param {boolean} inPlace - Whether to modify the object in place.
+ * @param {boolean} recursive - Whether to hash recursively.
+ * @returns {Record<string, any>} The hashed JSON object.
+ */
+function addHashes(
+  json,
+  updateExistingHashes = true,
+  floatingPointPrecision = 10,
+  hashLength = 22,
+  inPlace = false,
+  recursive = true,
+) {
+  return new JsonHash(
+    hashLength,
+    floatingPointPrecision,
+    updateExistingHashes,
+    recursive,
+  ).applyTo(json, inPlace);
+}
+
+/**
+ * Class for adding hashes to JSON objects.
+ */
+class JsonHash {
+  /**
+   * Constructor.
+   * @param {number} hashLength - The length of the hash in bytes.
+   * @param {number} floatingPointPrecision - Precision for floating point numbers.
+   * @param {boolean} updateExistingHashes - Replace existing hashes.
+   * @param {boolean} recursive - Recursively iterate into child objects.
+   */
+  constructor(
+    hashLength = 22,
+    floatingPointPrecision = 10,
+    updateExistingHashes = true,
+    recursive = true,
+  ) {
+    this.hashLength = hashLength;
+    this.floatingPointPrecision = floatingPointPrecision;
+    this.updateExistingHashes = updateExistingHashes;
+    this.recursive = recursive;
+  }
+
+  /**
+   * Adds hashes to the JSON object.
+   * @param {object} json - The JSON object.
+   * @param {boolean} inPlace - Modify the object in place.
+   * @returns {object} The hashed JSON object.
+   */
+  applyTo(json, inPlace = false) {
+    const copy = inPlace ? json : this._copyJson(json);
+    this._addHashesToObject(copy, this.recursive);
+    return copy;
+  }
+
+  /**
+   * Adds hashes to a JSON string.
+   * @param {string} jsonString - The JSON string.
+   * @returns {string} The JSON string with hashes.
+   */
+  applyToString(jsonString) {
+    const json = JSON.parse(jsonString);
+    const hashedJson = this.applyTo(json, true);
+    return JSON.stringify(hashedJson);
+  }
+
+  /**
+   * Calculates a SHA-256 hash of a string with base64 URL encoding.
+   * @param {string} string - The input string.
+   * @param {number} hashLength - The length of the hash.
+   * @returns {string} The hash.
+   */
+  calcHash(string, hashLength = 22) {
+    const hash = crypto
+      .createHash('sha256')
+      .update(string, 'utf8')
+      .digest('base64url');
+
+    return hash.substring(0, hashLength);
+  }
+
+  /**
+   * Recursively adds hashes to an object.
+   * @private
+   * @param {{ [key: string]: any }} obj - The object.
+   * @param {boolean} recursive - Whether to hash recursively.
+   */
+  _addHashesToObject(obj, recursive) {
+    if (
+      !this.updateExistingHashes &&
+      Object.prototype.hasOwnProperty.call(obj, '_hash')
+    ) {
+      return;
+    }
+
+    for (const [, value] of Object.entries(obj)) {
+      if (
+        typeof value === 'object' &&
+        !Array.isArray(value) &&
+        value !== null
+      ) {
+        if (value['_hash'] && !recursive) {
+          continue;
+        }
+        this._addHashesToObject(value, recursive);
+      } else if (Array.isArray(value)) {
+        this._processList(value);
+      }
+    }
+
+    /** @type {{[key: string]: any}} */
+    const objToHash = {};
+
+    for (const [key, value] of Object.entries(obj)) {
+      if (key === '_hash') continue;
+
+      if (typeof value === 'object' && value !== null) {
+        objToHash[key] = value['_hash'];
+      } else if (Array.isArray(value)) {
+        objToHash[key] = this._flattenList(value);
+      } else if (typeof value === 'number') {
+        objToHash[key] = this._truncate(value, this.floatingPointPrecision);
+      } else {
+        objToHash[key] = value;
+      }
+    }
+
+    const sortedJsonString = this._jsonString(objToHash);
+    obj['_hash'] = this.calcHash(sortedJsonString);
+  }
+
+  /**
+   * Processes a list recursively.
+   * @private
+   * @param {Array<any>} list - The list to process.
+   */
+  _processList(list) {
+    for (const item of list) {
+      if (typeof item === 'object' && !Array.isArray(item) && item !== null) {
+        this._addHashesToObject(item, this.recursive);
+      } else if (Array.isArray(item)) {
+        this._processList(item);
+      }
+    }
+  }
+
+  /**
+   * Flattens a list for hashing.
+   * @private
+   * @param {Array<any>} list - The list to flatten.
+   * @returns {Array<any>} The flattened list.
+   */
+  _flattenList(list) {
+    return list.map((item) => {
+      if (typeof item === 'object' && !Array.isArray(item) && item !== null) {
+        return item['_hash'];
+      } else if (Array.isArray(item)) {
+        return this._flattenList(item);
+      } else {
+        return item;
+      }
+    });
+  }
+
+  /**
+   * Copies a JSON object.
+   * @private
+   * @param {object} json - The JSON object.
+   * @returns {object} The copy.
+   */
+  _copyJson(json) {
+    return JSON.parse(JSON.stringify(json));
+  }
+
+  /**
+   * Truncates a number to the specified precision.
+   * @private
+   * @param {number} value - The number.
+   * @param {number} precision - The precision.
+   * @returns {number} The truncated number.
+   */
+  _truncate(value, precision) {
+    if (Number.isInteger(value)) {
+      return value;
+    }
+
+    const parts = value.toString().split('.');
+    const integerPart = parts[0];
+    const decimalPart = parts[1] || '';
+
+    var truncatedDecimalPart =
+      decimalPart.length > precision
+        ? decimalPart.substring(0, precision)
+        : decimalPart;
+
+    // Remove trailing zeros
+    if (truncatedDecimalPart.endsWith('0')) {
+      truncatedDecimalPart = truncatedDecimalPart.replaceAll(/0+$/, '');
+    }
+
+    if (!truncatedDecimalPart) {
+      return parseInt(integerPart);
+    }
+
+    const result = `${integerPart}.${truncatedDecimalPart}`;
+    return parseFloat(result);
+  }
+  /**
+   * Converts a JSON object to a string.
+   * @private
+   * @param {object} obj - The JSON object.
+   * @returns {string} The JSON string.
+   */
+  _jsonString(obj) {
+    return JSON.stringify(obj, Object.keys(obj).sort());
+  }
+
+  /**
+   * For test purposes, we are exposing these private methods
+   */
+  get privateMethods() {
+    return {
+      _copyJson: this._copyJson,
+      _truncate: this._truncate,
+      _jsonString: this._jsonString,
+    };
   }
 }
+
+export { JsonHash, addHashes };
--- src/gg-json-hash.spec.js
+++ src/gg-json-hash.spec.js
@@ -1,9 +1,153 @@
-import { describe, it } from 'vitest';
-import { ggJsonHash } from './gg-json-hash.js';
+// @license
+// Copyright (c) 2019 - 2024 Dr. Gabriel Gatzsche. All Rights Reserved.
+//
+// Use of this source code is governed by terms that can be
+// found in the LICENSE file in the root of this package.
 
-describe('ggJsonHash', () => {
-  it('should log the correct message to the console', () => {
-    ggJsonHash('test');
-    ggJsonHash('10');
+import { beforeAll, describe, expect, test } from 'vitest';
+import { addHashes, JsonHash } from './gg-json-hash'; // Implement these utilities
+
+const jh = new JsonHash();
+
+describe('JsonHash', () => {
+  describe('with a simple json', () => {
+    describe('containing only one key value pair', () => {
+      test('with a string value', () => {
+        const json = addHashes({ key: 'value' });
+        expect(json.key).toBe('value');
+        const expectedHash = jh.calcHash('{"key":"value"}');
+        expect(json._hash).toBe(expectedHash);
+        expect(json._hash).toBe('5Dq88zdSRIOcAS-WM_lYYt');
+      });
+
+      test('with an int value', () => {
+        const json = addHashes({ key: 1 });
+        expect(json.key).toBe(1);
+        const expectedHash = jh.calcHash('{"key":1}');
+        expect(json._hash).toBe(expectedHash);
+        expect(json._hash).toBe('t4HVsGBJblqznOBwy6IeLt');
+      });
+
+      test('with double value without commas', () => {
+        const json = addHashes({ key: 1.0 });
+        expect(json.key).toBe(1);
+        const expectedHash = jh.calcHash('{"key":1}');
+        expect(json._hash).toBe(expectedHash);
+        expect(json._hash).toBe('t4HVsGBJblqznOBwy6IeLt');
+      });
+
+      test('with a bool value', () => {
+        const json = addHashes({ key: true });
+        expect(json.key).toBe(true);
+        const expectedHash = jh.calcHash('{"key":true}');
+        expect(json._hash).toBe(expectedHash);
+        expect(json._hash).toBe('dNkCrIe79x2dPyf5fywwYO');
+      });
+
+      test('with a long double value', () => {
+        const json = addHashes({ key: 1.01234567890123 });
+        const expectedHash = jh.calcHash('{"key":1.0123456789}');
+        expect(json._hash).toBe(expectedHash);
+        expect(json._hash).toBe('Cj6IqsbT9fSKfeVVkytoqA');
+      });
+
+      test('with a short double value', () => {
+        const json = addHashes({ key: 1.012 });
+        const expectedHash = jh.calcHash('{"key":1.012}');
+        expect(json._hash).toBe(expectedHash);
+        expect(json._hash).toBe('ppGtYoP5iHFqst5bPeAGMf');
+      });
+    });
+
+    test('existing _hash should be overwritten', () => {
+      const json = addHashes({
+        key: 'value',
+        _hash: 'oldHash',
+      });
+      expect(json.key).toBe('value');
+      const expectedHash = jh.calcHash('{"key":"value"}');
+      expect(json._hash).toBe(expectedHash);
+      expect(json._hash).toBe('5Dq88zdSRIOcAS-WM_lYYt');
+    });
+
+    describe('containing floating point numbers', () => {
+      test('truncates the floating point numbers to "hashFloatingPrecision" 10 decimal places', () => {
+        const hash0 = addHashes({ key: 1.012345678901 }, true, 9)._hash;
+
+        const hash1 = addHashes({ key: 1.012345678901234 }, true, 9)._hash;
+
+        const expectedHash = jh.calcHash('{"key":1.012345678}');
+        expect(hash0).toBe(hash1);
+        expect(hash0).toBe(expectedHash);
+        expect(hash0).toBe('KTqI1AvWb3gI6dYA5HPPMx');
+      });
+    });
+
+    describe('containing three key value pairs', () => {
+      const json0 = {
+        a: 'value',
+        b: 1.0,
+        c: true,
+      };
+
+      const json1 = {
+        b: 1.0,
+        a: 'value',
+        c: true,
+      };
+
+      /** @type {Record<string, any>} */
+      let j0;
+
+      /** @type {Record<string, any>} */
+      let j1;
+
+      beforeAll(() => {
+        j0 = addHashes(json0);
+        j1 = addHashes(json1);
+      });
+
+      test('should create a string of key value pairs and hash it', () => {
+        const expectedHash = jh.calcHash('{"a":"value","b":1,"c":true}');
+
+        expect(j0._hash).toBe(expectedHash);
+        expect(j1._hash).toBe(expectedHash);
+      });
+
+      test('should work independent of key order', () => {
+        expect(j0).toEqual(j1);
+        expect(j0._hash).toBe(j1._hash);
+        expect(true.toString()).toBe('true');
+      });
+    });
+  });
+
+  describe('_truncate(double, precision)', () => {
+    // Get the truncate method from privateMethods
+    const truncate = jh.privateMethods['_truncate'];
+
+    test('truncates decimals but only if precision exceeds precision', () => {
+      expect(truncate(1.0, 5)).toBe(1);
+      expect(truncate(1, 5)).toBe(1);
+
+      expect(truncate(1.23456789, 2)).toBe(1.23);
+      expect(truncate(1.23456789, 3)).toBe(1.234);
+      expect(truncate(1.23456789, 4)).toBe(1.2345);
+      expect(truncate(1.23456789, 5)).toBe(1.23456);
+      expect(truncate(1.23456789, 6)).toBe(1.234567);
+      expect(truncate(1.23456789, 7)).toBe(1.2345678);
+      expect(truncate(1.23456789, 8)).toBe(1.23456789);
+      expect(truncate(1.12, 1)).toBe(1.1);
+      expect(truncate(1.12, 2)).toBe(1.12);
+      expect(truncate(1.12, 3)).toBe(1.12);
+      expect(truncate(1.12, 4)).toBe(1.12);
+    });
+
+    test('does not add additional decimals', () => {
+      expect(truncate(1.0, 0)).toBe(1);
+      expect(truncate(1.0, 1)).toBe(1.0);
+      expect(truncate(1.0, 2)).toBe(1.0);
+      expect(truncate(1.0, 3)).toBe(1.0);
+    });
   });
 });
